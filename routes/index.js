657ebe359afe92381b54ac41334057e8a41df8d8
Simone Taeggi <startae14@gmail.com>
2019-03-19 14:23:13 +0100

    Add some routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const tae = {
+    name: 'Simone',
+    age: 34,
+    cool: true
+  };
+  //res.send('Hey! It works!');
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  // create the reverse of the string
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
